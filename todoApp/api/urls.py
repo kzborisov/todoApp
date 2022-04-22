f2af0ef108e9f26abbabc9f6a7e98e044bda9b29
kborisov <kborisov@lely.com>
2022-04-22 11:25:09 +0300

    Initial commit

---
+++ todoApp/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from todoApp.api import views
+
+urlpatterns = (
+    path('', views.api_overview, name='api-overview'),
+    path('task-list/', views.task_list, name='task-list'),
+    path('task-create/', views.task_create, name='task-create'),
+    path('task-details/<str:pk>/', views.task_details, name='task-details'),
+    path('task-update/<str:pk>/', views.task_update, name='task-update'),
+    path('task-delete/<str:pk>/', views.task_delete, name='task-delete'),
+)
---
+++ todoApp/api/views.py
@@ -0,0 +1,58 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from todoApp.api.models import Task
+from todoApp.api.serializers import TaskSerializer
+
+
+@api_view(['GET'])
+def api_overview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-details/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_details(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+
+    return Response('Item successfully deleted.')
